ea33d08733cfc01e9a553d545cc469187025e524
Dai <dai.williams@bbc.co.uk>
2015-08-02 22:13:13 +0100

    Added search box and configured maps

--- webapp/src/maps/initialise.js
+++ webapp/src/maps/initialise.js
@@ -1,16 +1,14 @@
 'use strict';
 
+const mapOptions = require('./map.options.js');
+
 module.exports = function() {
-  
+
   function initialize() {
-    let mapOptions = {
-      center: { lat: 51.477427, lng: -3.693906},
-      zoom: 8
-    };
-    let map = new google.maps.Map(document.getElementById('map-canvas'),
+    global.map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
   }
 
   google.maps.event.addDomListener(window, 'load', initialize);
-  
+
 };
---
+++ webapp/src/maps/map.options.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const MapOptions = {
+  center: { lat: 51.4652501, lng: -3.1696349},
+  zoom: 8,
+  disableDefaultUI: true,
+  scrollwheel: false,
+  disableDoubleClickZoom: true,
+  draggable: false
+};
+
+module.exports = MapOptions;
