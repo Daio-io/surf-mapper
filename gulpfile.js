b3a1a00395de8c97af380c468a7e0e9fc574d903
Dai <dai.williams@bbc.co.uk>
2015-08-02 13:34:48 +0100

    Webpack builds the maps js to initialise

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,18 @@
 
 const gulp = require('gulp');
 const webpack = require('webpack-stream');
+const del = require('del');
 
-gulp.task('build', function() {
-  return gulp.src('webapp/src/**/*.js')
+// Webpack js files into one bundle
+gulp.task('build',['clean'], function() {
+  return gulp.src('webapp/src/index.js')
     .pipe(webpack({output: {filename: 'build.js'}}))
     .pipe(gulp.dest('static/dist/js/'));
+});
+
+// Clean out the dist folders
+gulp.task('clean', function(){
+  del(['static/dist/js']);
 });
 
 gulp.task('default', ['build'], function() {});
---
+++ webapp/src/maps/initialise.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+module.exports = function() {
+  function initialize() {
+    var mapOptions = {
+      center: { lat: 51.477427, lng: -3.693906},
+      zoom: 8
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+  
+};
