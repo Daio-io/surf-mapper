95c026c07301cce4d91b4d99ed5ffd3f6ee0a35b
Dai <dai.williams@bbc.co.uk>
2015-08-05 23:06:23 +0100

    Moved css into webapp and created gulp minify task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 const gulp = require('gulp');
 const webpack = require('webpack-stream');
 const del = require('del');
+const minifyCss = require('gulp-minify-css');
+
 const webpackConf = {
   output: {filename: 'build.js'},
   module: {
@@ -13,15 +15,21 @@ const webpackConf = {
 };
 
 // Webpack js files into one bundle
-gulp.task('build', ['clean'], function() {
+gulp.task('build', ['clean', 'minify-css'], function() {
   return gulp.src('webapp/src/index.js')
     .pipe(webpack(webpackConf))
     .pipe(gulp.dest('static/dist/js/'));
 });
 
+gulp.task('minify-css', function() {
+  return gulp.src('webapp/css/*.css')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('static/dist/css/'));
+});
+
 // Clean out the dist folders
 gulp.task('clean', function() {
-  del(['static/dist/js']);
+  del(['static/dist/']);
 });
 
 gulp.task('default', ['build'], function() {
