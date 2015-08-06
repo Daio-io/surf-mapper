690fade4487cdea401cafb4a4247c339a5e484a1
Dai <dai.williams@bbc.co.uk>
2015-08-06 22:26:19 +0100

    adding wake up for heroku services

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require('http');
 const koa = require('koa');
 const settings = require('./app/conf/app.settings');
 const app = koa();
+const got = require('got');
 
 //** BOOTSTRAP MIDDLEWARE **//
 require('./app/conf/app.bootstrap')(app);
@@ -17,3 +18,7 @@ server.listen(settings.port, function() {
   console.log('Surf Map started on port:', settings.port);
 
 });
+
+// Hack since services are hosted on heroku - send a request to the status to wake them up
+got('http://beach-locator.herokuapp.com/status');
+got('http://beach-suggest.herokuapp.com/status');
