72b2b9b9c91fbbff0340a5630ac00148879ea5fd
Dai <dai.williams@bbc.co.uk>
2015-08-25 20:50:41 +0100

    refactor pin dropper

--- webapp/src/maps/pin.dropper.js
+++ webapp/src/maps/pin.dropper.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let markers = [];
+const icon = '/img/surfboard-icon.png';
 
 export var PinDropper = {
 
@@ -8,31 +9,51 @@ export var PinDropper = {
 
     _clearCurrentMarkers();
 
-    let infowindow = new google.maps.InfoWindow({
-      content: data.surfcard
-    });
-    let position = new google.maps.LatLng(data.lat, data.lng);
-
-    let icon = '/img/surfboard-icon.png';
-    let marker = new google.maps.Marker({
-      position: position,
-      map: global.map,
-      icon: icon,
-      animation: google.maps.Animation.DROP
-    });
-
+    let marker = _createMarker(data);
     markers.push(marker);
+    
+    let infoWindow = _createInfoWindow(data);
 
-    global.map.panTo(marker.getPosition());
     global.map.setZoom(7);
-    infowindow.open(global.map, marker);
+    global.map.panTo(marker.getPosition());
+    infoWindow.open(global.map, marker);
+
   }
 };
 
 function _clearCurrentMarkers() {
 
-  for (let i = 0; i < markers.length; i++){
+  for (let i = 0; i < markers.length; i++) {
     markers[i].setMap(null);
   }
   markers = [];
+}
+
+function _createMarker(data){
+  let position = new google.maps.LatLng(data.lat, data.lng);
+
+  let marker = new google.maps.Marker({
+    position: position,
+    map: global.map,
+    icon: icon,
+    animation: google.maps.Animation.DROP
+  });
+  
+  return marker;
+  
+}
+
+function _createInfoWindow(data){
+  let infowindow = new google.maps.InfoWindow({
+    content: data.surfcard
+  });
+  
+  google.maps.event.addListener(infowindow,
+    'closeclick', function() {
+      _clearCurrentMarkers();
+      $('.search-container').show();
+    });
+  
+  return infowindow;
+  
 }
