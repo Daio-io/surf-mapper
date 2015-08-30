51468ccec030a48abbf25796426ba819283eafd6
Dai <dai.williams@bbc.co.uk>
2015-08-30 12:55:01 +0100

    Use uglified js bundle in live

--- app/home/home.handlers.js
+++ app/home/home.handlers.js
@@ -3,13 +3,15 @@
 const mapsApiKey = process.env.MAPS_KEY;
 const tooly = require('tooly');
 const builder =  require('../lib/response.builder');
+const bundle = process.env.NODE_ENV === 'live' ? 'bundle.min.js' : 'bundle.js';
 
 exports.getHome = function *() {
 
   yield this.render('home',
     {
       title: 'Surf Mapper',
-      mapskey: mapsApiKey
+      mapskey: mapsApiKey,
+      bundle: bundle
     }
   );
 
