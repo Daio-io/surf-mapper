5dfc989078b3e0203b07895959163bf5a88b8d65
daveloper87 <dai.williams@bbc.co.uk>
2015-10-25 19:07:12 +0000

    Ensure map options is a frozen object

--- webapp/src/maps/map.options.js
+++ webapp/src/maps/map.options.js
@@ -1,9 +1,9 @@
 'use strict';
 
-export const mapOptions = {
+export const mapOptions = Object.freeze({
   center: { lat: 51.4652501, lng: -3.1696349 },
   zoom: 5,
   disableDefaultUI: true,
   scrollwheel: false,
   disableDoubleClickZoom: true
-};
+});
