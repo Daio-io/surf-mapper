fa7f567713e1546d785ea4f72e4a30738f358b75
Daio-io <dai.williams@bbc.co.uk>
2016-01-10 17:15:07 +0000

    Fix to use new locator

--- app/lib/locator.client.js
+++ app/lib/locator.client.js
@@ -13,7 +13,7 @@ module.exports = {
     
     return got('http://beach-locator.herokuapp.com/location/' + spotid)
       .then(function(data){
-      return data.body.response;
+      return data.body;
     });
 
   }
